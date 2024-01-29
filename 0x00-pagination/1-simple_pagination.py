88f07216b764b99840f0aa034b379455fe86fb8b
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2024-01-29 10:22:18 +0000

    fix_trailing_comma

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -55,8 +55,8 @@ class Server:
         Returns:
             List[List]: List of rows for the specified page.
         """
-        assert isinstance(page, int) and page > 0,
-        assert isinstance(page_size, int) and page_size > 0,
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
